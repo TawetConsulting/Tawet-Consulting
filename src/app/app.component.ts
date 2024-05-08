8702101cd332da04ae461d0207d41479da2b6260
Emillio Itai Marambo <emilliomarambo@gmail.com>
2024-05-09 00:29:51 +0200

    Changes to accommodate slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Subscription, interval, takeWhile } from 'rxjs';
+import { SwiperOptions } from 'swiper';
 
 interface Slide {
   image: string;
@@ -14,7 +15,7 @@ interface Slide {
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'Tawet Consulting';
   loading = true;
   titleState = "";
@@ -22,104 +23,43 @@ export class AppComponent implements AfterViewInit {
   
   isMenuOpen: boolean = false;
 
-  @ViewChild('typingText', { static: false }) typingText!: ElementRef;
-  @ViewChild('textContent', { static: false }) textContent!: ElementRef;
-
+  config: SwiperOptions = {
+    loop: true,
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false
+    }
+  };
+  
   slides: Slide[] = [
     {
-      image: '../../assets/images/backgrounds/welcome/beyond_bounderies.jpg',
+      image: '../../assets/images/backgrounds/welcome/finance.jpg',
       title: 'Go beyond boundaries',
-      subtitle: 'Mastering Risk, Finance, and Regulations for Strategic Excellence',
+      subtitle: 'Business Finance, and Regulations',
       text: 'Unlock the power of interdisciplinary expertise to conquer evolving risk landscapes and propel your business towards unparalleled growth.'
     },
     {
-      image: '../../assets/images/backgrounds/welcome/insight_unleshed.jpg',
+      image: '../../assets/images/backgrounds/welcome/data-business.jpg',
       title: 'Insight Unleashed',
-      subtitle: 'Transforming Business with Data Analytics and Intelligence',
+      subtitle: 'Data Analytics and Business Intelligence',
       text: 'Empower your organization with precise data-driven solutions, guiding strategic decisions and maximizing potential across industries'
     },
     {
-      image: '../../assets/images/backgrounds/welcome/precision_shield.jpg',
+      image: '../../assets/images/backgrounds/welcome/risk-management.jpg',
       title: 'Precision Shield',
-      subtitle: 'Expert Risk Management Solutions',
+      subtitle: 'Risk Management Solutions',
       text: 'Navigate financial risks seamlessly with our expert team, combining numerical optimization and strategic foresight to fortify your organization\'s resilience'
     },
   ];
 
-  currentSlideIndex = 0;
-  private slideSubscription!: Subscription;
-  
-  text = "";
-  count = 0;
-  intervalId!: any;
-  imageSrc ="";
-  itemTittle = "";
-  subtitle = "";
+  ngOnInit() {
+  }
 
-  ngAfterViewInit() {
-    this.text = "";
+  ngOnDestroy() {
   }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  startSlideShow() {
-    const slideInterval = interval(6000); 
-    this.slideSubscription = slideInterval.pipe(
-      takeWhile(() => true) 
-    ).subscribe(() => {
-      this.updateSlide();
-    });
-  }
 
-  ngOnInit() {
-    // setTimeout(() => {
-    //   this.loading = false;
-    // }, 15000); 
-    // window.addEventListener('scroll', this.handleScroll.bind(this));
-    // this.startSlideShow();
-  }
-
-  ngOnDestroy() {
-    this.slideSubscription.unsubscribe();
-  }
-
-  handleScroll() {
-    const scrollPosition = window.pageYOffset;
-    const header = document.getElementById('sticky-header');
-    if(header != null){
-      const stickyHeight = header.offsetHeight;
-      this.isSticky = scrollPosition > stickyHeight ;
-    }
-
-  }
-  updateSlide() {
-    const currentSlide = this.slides[this.currentSlideIndex];
-
-    // Update content
-    this.imageSrc = currentSlide.image;
-    this.textContent.nativeElement.textContent = currentSlide.text; 
-    this.text = currentSlide.text;
-    this.count = 0; 
-    this.startTyping();
-
-    // Update title and subtitle (if provided)
-    this.itemTittle = currentSlide.title;
-    this.subtitle = currentSlide.subtitle;
-
-    this.currentSlideIndex++;
-    if (this.currentSlideIndex >= this.slides.length) {
-      this.currentSlideIndex = 0; 
-    }
-  }
-  startTyping() {
-    this.intervalId = setInterval(() => {
-      
-      this.typingText.nativeElement.textContent = this.text.slice(0, this.count);
-      this.count++;
-      if (this.count === this.text.length) {
-        clearInterval(this.intervalId);
-      }
-    }, 50);
-  }
 }
