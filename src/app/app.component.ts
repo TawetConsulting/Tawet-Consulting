39cab30b9912959b40fa6f32c8abfa207c07fc06
Emillio Itai Marambo <emilliomarambo@gmail.com>
2024-05-18 21:39:19 +0200

    Update pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,11 @@ export class AppComponent implements AfterViewInit{
 
   config: SwiperOptions = {
     loop: true,
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
     autoplay: {
       delay: 7000,
       disableOnInteraction: false
